c004adbb5be86c7dd0df1c36784952b0424369c2
cvikupitz <cvikupitz@gmail.com>
2017-10-14 19:37:58 -0700

    Requests are now recognized.

--- duckchat/client.c
+++ duckchat/client.c
@@ -22,17 +22,64 @@
 
 static char username[USERNAME_MAX + 1];
 static int socket_fd;
+////FIXME = ERROR CHECK send()
 
 
 /**
  * FIXME
  */
+UNUSED static void client_join() {
+    puts("-- Reached client join...");
+}
+
+/**
+ * FIXME
+ */
+UNUSED static void client_leave() {
+    puts("-- Reached client leave...");
+}
+
+/**
+ * FIXME
+ */
+UNUSED static void client_say() {
+    puts("-- Reached client say...");
+}
+
+/**
+ * FIXME
+ */
+static void client_list() {
+    puts("-- Reached client list...");
+    struct request_list list_packet;
+    list_packet.req_type = REQ_LIST;
+    send(socket_fd, &list_packet, sizeof(list_packet), 0);
+}
+
+/**
+ * FIXME
+ */
+UNUSED static void client_who() {
+    puts("-- Reached client who...");
+}
+
+/**
+ * FIXME
+ */
+static void client_logout() {
+    puts("-- Reached client logout...");
+    struct request_logout logout_packet;
+    logout_packet.req_type = REQ_LOGOUT;
+    send(socket_fd, &logout_packet, sizeof(logout_packet), 0);
+}
+
+/**
+ * FIXME
+ */
 static void print_error(const char *msg) {
-    
     perror(msg);
     exit(-1);
 }
-
 
 /**
  * FIXME
@@ -50,7 +97,7 @@ int main(int argc, char *argv[]) {
 	fprintf(stderr, "Usage: %s server_socket server_port username\n", argv[0]);
 	exit(-1);
     }
-    
+   
     if ((server = gethostbyname(argv[1])) == NULL)
 	print_error("Error - Failed to locate the server.\n");
     port_num = atoi(argv[2]);
@@ -77,8 +124,27 @@ int main(int argc, char *argv[]) {
     send(socket_fd, &login_packet, sizeof(login_packet), 0);
 
     while (1) {
-	fprintf(stdout, ">");
+
+	fputs(">", stdout);
 	fgets(buffer, sizeof(buffer), stdin);
+	buffer[strlen(buffer) - 1] = '\0';
+
+	if (buffer[0] == '/') {
+	    if (strcmp(buffer + 1, "join") == 0) {
+		client_join();
+	    } else if (strcmp(buffer + 1, "leave") == 0) {
+		client_leave();
+	    } else if (strcmp(buffer + 1, "list") == 0) {
+		client_list();
+	    } else if (strcmp(buffer + 1, "who") == 0) {
+		client_who();
+	    } else if (strcmp(buffer + 1, "exit") == 0) {
+		client_logout();
+		break;
+	    } else {
+		fputs("*Unknown command\n", stdout);
+	    }
+	}
     }
 
     close(socket_fd);
